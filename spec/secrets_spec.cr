48fcaa987998869dc1bdd0b9f1c7cd2c5aebc8b3
Sam Johnson <sam@durosoft.com>
2020-05-23 16:16:42 -0400

    refactor

--- spec/secrets_spec.cr
+++ spec/secrets_spec.cr
@@ -1,22 +1,20 @@
 require "./spec_helper"
 
-module Secrets
-  describe SecretStore do
-    it "generates appropriate encryption key" do
-      env = SecretStore.new("production")
-      env.@encryption_key.size.should eq SecretStore::KEY_SIZE
-      env.@aes.@key.size.should eq SecretStore::KEY_SIZE
-    end
+describe SecretStore do
+  it "generates appropriate encryption key" do
+    env = SecretStore.new("production")
+    env.@encryption_key.size.should eq SecretStore::KEY_SIZE
+    env.@aes.@key.size.should eq SecretStore::KEY_SIZE
+  end
 
-    it "encryption/decryption round trip" do
-      env = SecretStore.new("development")
-      env["API_KEY"] = "alskdjfas0dj90fjajsdf"
-      env["SOME_KEY"] = "a8sd89fj8"
-      orig_yaml = env.data.to_yaml
-      encoded = env.encode
-      encoded.should_not eq orig_yaml
-      env = SecretStore.new("development", env.@encryption_key, encoded)
-      env.data.to_yaml.should eq orig_yaml
-    end
+  it "encryption/decryption round trip" do
+    env = SecretStore.new("development")
+    env["API_KEY"] = "alskdjfas0dj90fjajsdf"
+    env["SOME_KEY"] = "a8sd89fj8"
+    orig_yaml = env.data.to_yaml
+    encoded = env.encode
+    encoded.should_not eq orig_yaml
+    env = SecretStore.new("development", env.@encryption_key, encoded)
+    env.data.to_yaml.should eq orig_yaml
   end
 end
--- src/secrets.cr
+++ src/secrets.cr
@@ -2,13 +2,58 @@ require "aes"
 require "yaml"
 require "file_utils"
 
-module Secrets
-  @@secret_stores : Hash(String, SecretStore) = Hash(String, SecretStore).new
+class SecretStore
+  AES_BITS = 256
+  KEY_SIZE = 32
+  IV_SIZE = 32
+
+  getter name : String
+  setter encryption_key : String
+  getter data : Hash(String, String)
+  @aes : AES
+
+  def initialize(@name)
+    @data = Hash(String, String).new
+    @aes = AES.new(AES_BITS)
+    @encryption_key = String.new(@aes.@key)
+  end
+
+  def initialize(@name, @encryption_key, data : String)
+    data = data.as_slice
+    iv = data[0..(IV_SIZE - 1)]
+    key = @encryption_key.as_slice
+    data = data[IV_SIZE..]
+    @aes = AES.new(key, iv, AES_BITS)
+    decrypted = String.new(@aes.decrypt(data))
+    yaml = YAML.parse(decrypted)
+    @data = yaml.as_h.map { |k, v| [k.to_s, v.to_s] }.to_h
+  end
+
+  def encode
+    mem = IO::Memory.new
+    mem.write(@aes.iv)
+    mem.write(@aes.encrypt(data.to_yaml))
+    String.new(mem.to_slice)
+  end
+
+  def [](key : Symbol | String)
+    data[key.to_s]
+  end
+
+  def []?(key : Symbol | String)
+    data[key.to_s]?
+  end
+
+  def []=(key : Symbol | String, value : Symbol | String)
+    data[key.to_s] = value.to_s
+  end
+
+  @@stores : Hash(String, SecretStore) = Hash(String, SecretStore).new
   class_property default_stores_dir : String = "./secrets"
   class_property default_keys_dir : String = "."
 
-  def self.register_store(name : String, store_path : String? = nil, key_path : String? = nil)
-    name = name.downcase.snake_case
+  def self.register(name : Symbol | String, store_path : String? = nil, key_path : String? = nil)
+    name = name.to_s.downcase
     store_path ||= Path[default_stores_dir].join("#{name}_secrets.yml").to_s
     key_path ||= Path[default_keys_dir].join(".#{name}_secret_key").to_s
     stores_dir = Path[store_path].parent.to_s
@@ -22,53 +67,16 @@ module Secrets
     else
       store = SecretStore.new(name)
     end
-    @@secret_stores[name] = store
+    @@stores[name] = store
   end
 
-  class SecretStore
-    AES_BITS = 256
-    KEY_SIZE = 32
-    IV_SIZE = 32
+  def self.[](name : Symbol | String)
+    name = name.to_s.downcase
+    @@stores[name]
+  end
 
-    getter name : String
-    setter encryption_key : String
-    getter data : Hash(String, String)
-    @aes : AES
-
-    def initialize(@name)
-      @data = Hash(String, String).new
-      @aes = AES.new(AES_BITS)
-      @encryption_key = String.new(@aes.@key)
-    end
-
-    def initialize(@name, @encryption_key, data : String)
-      data = data.as_slice
-      iv = data[0..(IV_SIZE - 1)]
-      key = @encryption_key.as_slice
-      data = data[IV_SIZE..]
-      @aes = AES.new(key, iv, AES_BITS)
-      decrypted = String.new(@aes.decrypt(data))
-      yaml = YAML.parse(decrypted)
-      @data = yaml.as_h.map { |k, v| [k.to_s, v.to_s] }.to_h
-    end
-
-    def encode
-      mem = IO::Memory.new
-      mem.write(@aes.iv)
-      mem.write(@aes.encrypt(data.to_yaml))
-      String.new(mem.to_slice)
-    end
-
-    def [](key : Symbol | String)
-      data[key.to_s]
-    end
-
-    def []?(key : Symbol | String)
-      data[key.to_s]?
-    end
-
-    def []=(key : Symbol | String, value : Symbol | String)
-      data[key.to_s] = value.to_s
-    end
+  def self.[]?(name : Symbol | String)
+    name = name.to_s.downcase
+    @@stores[name]?
   end
 end
