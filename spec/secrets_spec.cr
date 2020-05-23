07fca4f1eec1c03bd6da4af58980ae7462a417de
Sam Johnson <sam@durosoft.com>
2020-05-23 05:42:54 -0400

    encryption round trip working

--- spec/secrets_spec.cr
+++ spec/secrets_spec.cr
@@ -1,9 +1,22 @@
 require "./spec_helper"
 
-describe Secrets do
-  # TODO: Write tests
+module Secrets
+  describe SecretEnvironment do
+    it "generates appropriate encryption key" do
+      env = SecretEnvironment.new("production")
+      env.@encryption_key.size.should eq SecretEnvironment::KEY_SIZE
+      env.@aes.@key.size.should eq SecretEnvironment::KEY_SIZE
+    end
 
-  it "works" do
-    false.should eq(true)
+    it "encryption/decryption round trip" do
+      env = SecretEnvironment.new("development")
+      env["API_KEY"] = "alskdjfas0dj90fjajsdf"
+      env["SOME_KEY"] = "a8sd89fj8"
+      orig_yaml = env.data.to_yaml
+      encoded = env.encode
+      encoded.should_not eq orig_yaml
+      env = SecretEnvironment.new("development", env.@encryption_key, encoded)
+      env.data.to_yaml.should eq orig_yaml
+    end
   end
 end
--- src/secrets.cr
+++ src/secrets.cr
@@ -1,28 +1,51 @@
 require "aes"
+require "yaml"
 
 module Secrets
   class SecretEnvironment
+    AES_BITS = 256
+    KEY_SIZE = 32
+    IV_SIZE = 32
+
     getter name : String
     setter encryption_key : String
     getter data : Hash(String, String)
+    @aes : AES
 
     def initialize(@name)
-      @encryption_key = Random::Secure.hex(64)
       @data = Hash(String, String).new
+      @aes = AES.new(AES_BITS)
+      @encryption_key = String.new(@aes.@key)
     end
 
     def initialize(@name, @encryption_key, data : String)
+      data = data.as_slice
+      iv = data[0..(IV_SIZE - 1)]
+      key = @encryption_key.as_slice
+      data = data[IV_SIZE..]
+      @aes = AES.new(key, iv, AES_BITS)
+      decrypted = String.new(@aes.decrypt(data))
+      yaml = YAML.parse(decrypted)
+      @data = yaml.as_h.map { |k, v| [k.to_s, v.to_s] }.to_h
+    end
 
+    def encode
+      mem = IO::Memory.new
+      mem.write(@aes.iv)
+      mem.write(@aes.encrypt(data.to_yaml))
+      String.new(mem.to_slice)
     end
 
     def [](key : Symbol | String)
-      key = key.to_s
-      data[key]
+      data[key.to_s]
     end
 
     def []?(key : Symbol | String)
-      key = key.to_s
-      data[key]?
+      data[key.to_s]?
+    end
+
+    def []=(key : Symbol | String, value : Symbol | String)
+      data[key.to_s] = value.to_s
     end
   end
 end
